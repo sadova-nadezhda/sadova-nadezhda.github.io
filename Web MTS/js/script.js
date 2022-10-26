db0d04018642f580053cd27a9529353fe53efdb8
worldofhope <n.sadova98@gmail.com>
2022-10-26 19:07:58 +0600

    update table MTS

--- Web MTS/js/script.js
+++ Web MTS/js/script.js
@@ -448,3 +448,42 @@ if(product){
 
   });
 }
+
+
+
+
+
+var tabledata = [
+  {id:1, name:"Billy Bob", age:12, gender:"male", height:95, col:"red", dob:"14/05/2010"},
+  {id:2, name:"Jenny Jane", age:42, gender:"female", height:142, col:"blue", dob:"30/07/1954"},
+  {id:3, name:"Steve McAlistaire", age:35, gender:"male", height:176, col:"green", dob:"04/11/1982"},
+];
+
+var tableColumns = [
+  {title:"Name", field:"name"},
+  {title:"Gender", field:"gender"},
+  {title:"Favourite Color", field:"col"},
+  {title:"Date Of Birth", field:"dob"},
+  {title:"Driver", field:"car"},
+  // {title:"Date Of Birth", field:"dob", hozAlign:"center", sorter:"date"},
+  // {title:"Driver", field:"car", hozAlign:"center", formatter:"tickCross"},
+];
+
+
+//Build Tabulator
+var table = new Tabulator("#example-table", {
+  data: tabledata,
+  columns: tableColumns,
+  columnDefaults:{
+    width:200,
+    resizable:false
+  },
+  pagination:"local",
+  paginationSize: 1,
+  paginationSizeSelector:[1, 3, 6],
+  paginationCounter:"rows",
+  layout:"fitColumns",
+  movableColumns:false,
+  resizable:false
+
+});
