dd683ccb90ca0437566b5d3939b0c40c5833e7eb
worldofhope <n.sadova98@gmail.com>
2022-12-26 18:33:29 +0600

    update gulp

--- gulp-2022/dist/js/app.js
+++ gulp-2022/dist/js/app.js
@@ -1,3 +1,21 @@
 // import * as flsFunctions from "./modules/functions.js";
 // flsFunctions.isWebp();
 
+window.addEventListener('load', function() {
+
+  let password = document.querySelector('.cabinet__pass');
+  let eyeCLs = password.querySelector('.icon ');
+  let eyeOpn = password.querySelector('.icon-view');
+  let inpPass = password.closest('.cabinet__input').querySelector('input');
+
+  if(password){
+    password.addEventListener('click', () =>{
+      inpPass.classList.toggle('view');
+      if(inpPass.classList.contains('view')){
+        inpPass.type = 'text';
+      } else {
+        inpPass.type = 'password';
+      }
+    });
+  }
+});
