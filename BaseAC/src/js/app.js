4474b72f4f072eae69f8b6f5521d6b82c9504bae
worldofhope <n.sadova98@gmail.com>
2023-01-06 16:52:32 +0600

    update

--- BaseAC/src/js/app.js
+++ BaseAC/src/js/app.js
@@ -99,4 +99,48 @@ window.addEventListener('load', function() {
     });
   });
 
+  $('.inner__slider_main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    nextArrow: '<button type="button" class="slick_arrow slick_next"><svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="-0.5" y="0.5" width="39" height="39" rx="4.5" transform="matrix(-1 0 0 1 39 0)" fill="#48A9A6" stroke="#B4C6D0"/><path d="M20.6667 27.6327L19.475 26.5903L25.7917 20.898L9 20.898L9 19.4014L25.7917 19.4014L19.475 13.7345L20.6667 12.6667L29 20.1497L20.6667 27.6327Z" fill="#F9F8FE"/></svg></button>',
+    prevArrow: '<button type="button" class="slick_arrow slick_prev"><svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="0.5" y="0.5" width="39" height="39" rx="4.5" fill="#48A9A6" stroke="#B4C6D0"/><path d="M19.3333 27.6327L20.525 26.5903L14.2083 20.898L31 20.898L31 19.4014L14.2083 19.4014L20.525 13.7345L19.3333 12.6667L11 20.1497L19.3333 27.6327Z" fill="#F9F8FE"/></svg></button>',
+    dots: false,
+    fade: true,
+    asNavFor: '.inner__slider',
+    responsive: [
+      {
+          breakpoint: 600,
+          settings: {
+            arrows: false
+          }
+      },
+  ]
+  });
+  $('.inner__slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.inner__slider_main',
+    arrows: false,
+    dots: false,
+    focusOnSelect: true,
+    // centerMode: true,
+    responsive: [
+      {
+          breakpoint: 1100,
+          settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1
+          }
+      },
+      {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+      },
+  ]
+  });
 });
