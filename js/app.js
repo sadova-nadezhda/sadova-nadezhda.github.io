e778dfd8264ad814e3ff68fa3a7ab635fded2995
worldofhope <n.sadova98@gmail.com>
2023-02-14 13:58:07 +0600

    update

--- js/app.js
+++ js/app.js
@@ -43,6 +43,114 @@ if(menu){
   })
 }
 
+document.addEventListener('load', function() {
+  const c = document.getElementById('canvas');
+  const ctx = c.getContext('2d');
+  const MAX_PARTICLES = 200;
+  
+  const mouse = {
+    x: 0,
+    y: 0,
+  };
+  
+  c.width = window.innerWidth;
+  c.height = window.innerHeight;
+  
+  function random(min, max) {
+    return (Math.random() * (max - min) + min);
+  }
+  
+  function distance(p1, p2) {
+    return Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
+  }
+  
+  class Particle {
+    constructor() {
+      this.x = random(0, c.width);
+      this.y = random(0, c.height);
+      this.vx = random(-0.5, 0.5);
+      this.vy = random(-0.5, 0.5);
+      this.size = random(1, 5);
+    }
+    update() {
+      this.x += this.vx;
+      this.y += this.vy;
+      if (this.x < 0) {
+        this.x = c.width;
+      }
+      if (this.x > c.width) {
+        this.x = 0;
+      }
+      if (this.y < 0) {
+        this.y = c.height;
+      }
+      if (this.y > c.height) {
+        this.y = 0;
+      }
+    }
+  }
+  
+  const particles = [];
+  
+  for (let i = 0; i < MAX_PARTICLES; i++) {
+    particles.push(new Particle());
+  }
+  
+  function update() {
+    particles.forEach((particle) => {
+      particle.update();
+    });
+  }
+  
+  function render() {
+    particles.forEach((particle) => {
+      particles.forEach((particle2) => {
+        const d = distance(particle, particle2)
+        if (d < 100) {
+          ctx.fillStyle = '#fff';
+          ctx.strokeStyle = '#fff';
+          const d2 = distance(mouse, particle);
+          if (d2 < 300) {
+            ctx.globalAlpha = (1000 / d2) / 100 ;
+            ctx.beginPath();
+            ctx.moveTo(particle.x, particle.y);
+            ctx.lineTo(particle2.x, particle2.y);
+            ctx.stroke();
+          }
+        }
+      });
+      ctx.fillStyle = 'black';
+      ctx.beginPath();
+      ctx.arc(particle.x, particle.y, particle.size / 2, 0, Math.PI*2, true);
+      ctx.fill();
+    });
+  }
+  
+  function loop() {
+    requestAnimationFrame(loop);
+    ctx.clearRect(0, 0, c.width, c.height);
+    update();
+    render();
+  }
+  
+  function init() {
+    mouse.x = c.width / 2;
+    mouse.y = c.height / 2;
+    loop();
+  }
+  
+  window.addEventListener('mousemove', (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+  });
+  
+  window.addEventListener('resize', (e) => {
+    c.width = window.innerWidth;
+  });
+  
+  init();
+})
+
 /*Show card services text*/
 let cardMore = document.querySelectorAll('.card__more');
 if(cardMore) {
@@ -175,141 +283,3 @@ if (popup) {
   });
 }
 
-/*canvas*/
-const c = document.getElementById('canvas');
-const ctx = c.getContext('2d');
-const MAX_PARTICLES = 200;
-
-const c2 = document.getElementById('canvas-2');
-const ctx2 = c2.getContext('2d');
-const MAX_PARTICLES2 = 200;
-
-const mouse = {
-  x: 0,
-  y: 0 
-};
-
-
-c.width = window.innerWidth;
-c.height = window.innerHeight;
-
-
-
-c2.width = document.querySelector('.offer__container').offsetWidth;
-c2.height = document.querySelector('.offer__container').offsetHeight;
-
-function random(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function distance(p1, p2) {
-  return Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
-}
-
-class Particle {
-  constructor() {
-    this.x = random(0, c.width);
-    this.y = random(0, c.height);
-    this.vx = random(-0.5, 0.5);
-    this.vy = random(-0.5, 0.5);
-    this.size = random(1, 3);
-  }
-  update() {
-    this.x += this.vx;
-    this.y += this.vy;
-    if (this.x < 0) {
-      this.x = c.width;
-      this.x = c2.width;
-    }
-    if (this.x > c.width || this.x > c2.width) {
-      this.x = 0;
-    }
-    if (this.y < 0) {
-      this.y = c.height;
-      this.y = c2.height;
-    }
-    if (this.y > c.height || this.y > c2.height) {
-      this.y = 0;
-    }
-  }}
-
-
-const particles = [];
-
-for (let i = 0; i < MAX_PARTICLES; i++) {
-  particles.push(new Particle());
-}
-for (let i = 0; i < MAX_PARTICLES2; i++) {
-  particles.push(new Particle());
-}
-
-function update() {
-  particles.forEach(particle => {
-    particle.update();
-  });
-}
-
-function render() {
-  particles.forEach(particle => {
-    particles.forEach(particle2 => {
-      const d = distance(particle, particle2);
-      if (d < 100) {
-        ctx.fillStyle = '#DCBB63';
-        ctx.strokeStyle = '#DCBB63';
-        ctx2.fillStyle = '#DCBB63';
-        ctx2.strokeStyle = '#DCBB63';
-        const d2 = distance(mouse, particle);
-        if (d2 < 300) {
-          ctx.globalAlpha = (1000 / d2) / 100;
-          ctx.beginPath();
-          ctx.moveTo(particle.x, particle.y);
-          ctx.lineTo(particle2.x, particle2.y);
-          ctx.stroke();
-
-          ctx2.globalAlpha = (1000 / d2) / 100;
-          ctx2.beginPath();
-          ctx2.moveTo(particle.x, particle.y);
-          ctx2.lineTo(particle2.x, particle2.y);
-          ctx2.stroke();
-        }
-      }
-    });
-    ctx.fillStyle = '#000';
-    ctx.beginPath();
-    ctx.arc(particle.x, particle.y, particle.size/2 , 0, Math.PI * 2, true);
-    ctx.fill();
-
-    ctx2.fillStyle = '#DCBB63';
-    ctx2.beginPath();
-    ctx2.arc(particle.x, particle.y, particle.size/2 , 0, Math.PI * 2, true);
-    ctx2.fill();
-  });
-}
-
-function loop() {
-  requestAnimationFrame(loop);
-  ctx.clearRect(0, 0, c.width, c.height);
-  ctx2.clearRect(0, 0, c2.width, c2.height);
-  update();
-  render();
-}
-
-function init() {
-  mouse.x = c.width / 2;
-  mouse.y = c.height / 2;
-  mouse.x = c2.width / 2;
-  mouse.y = c2.height / 2;
-  loop();
-}
-
-window.addEventListener('mousemove', e => {
-  mouse.x = e.x;
-  mouse.y = e.y;
-});
-
-window.addEventListener('resize', e => {
-  c.width = window.innerWidth;
-  c2.width = window.innerWidth;
-});
-
-init();
