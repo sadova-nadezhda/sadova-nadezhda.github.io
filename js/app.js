6095085ac491b492b3b28bb42e3b09da0ee2a0cf
worldofhope <n.sadova98@gmail.com>
2023-04-10 15:09:31 +0600

    update

--- js/app.js
+++ js/app.js
@@ -118,27 +118,17 @@ window.addEventListener("load", function () {
     },
   });
 
-  let toggles = document.querySelectorAll('[data-toggle]'),
-    i = toggles.length;
+  let toggles = document.querySelectorAll('[data-toggle]'), i = toggles.length;
 
   for (i;i--;) {
-    
     toggles[i].addEventListener('click', function(e) {
-    
       e.preventDefault();
-
-      var i = toggles.length;
-
+      let i = toggles.length;
       for (i;i--;) {
-
         toggles[i].parentNode.classList.remove('active');
-
       }
-
       this.parentNode.classList.add('active');
-    
     }, false);
-    
   }
 });
 
