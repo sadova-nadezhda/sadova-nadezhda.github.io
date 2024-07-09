7c5c7ab9324852024894520942a529fcb9969308
worldofhope <n.sadova98@gmail.com>
2024-07-09 14:17:52 +0500

    update mask tel

--- js/app.js
+++ js/app.js
@@ -135,4 +135,22 @@ window.addEventListener("load", function () {
     });
   }
 
+  // input mask tel
+  $.fn.setCursorPosition = function (pos) {
+    if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select();
+    }
+  };
+  $('input[type="tel"]')
+    .click(function () {
+      $(this).setCursorPosition(3);
+    })
+    .mask("+7 (999) 999 99 99");
+
 });
