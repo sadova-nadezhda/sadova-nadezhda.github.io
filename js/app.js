a57671f9373a17b77d2c9bfb9c1d2582f90b79f9
worldofhope <n.sadova98@gmail.com>
2024-08-29 15:55:24 +0500

    project omoikiri

--- js/app.js
+++ js/app.js
@@ -1,73 +1,82 @@
-// Global Vars
-var header = document.querySelector("header");
-var sectionTop = document.querySelector('.section-top');
-
-// Functions
-function addPadTop(header, section) {
-  let headerHeight = header.offsetHeight;
-  section.style.paddingTop = `${headerHeight}px`;
-}
+let header = document.querySelector('.header');
 
 window.addEventListener("load", function () {
 
-  // Header 
-  let link = document.querySelector(".header__burger");
-  let menu = document.querySelector(".header__nav");
-  if (menu) {
-    link.addEventListener(
-      "click",
-      function () {
-        link.classList.toggle("active");
-        menu.classList.toggle("open");
-      },
-      false
-    );
-    window.addEventListener("scroll", () => {
-      if (menu.classList.contains("open")) {
-        link.classList.remove("active");
-        menu.classList.remove("open");
-      }
-    });
-    document.addEventListener("click", (e) => {
-      let target = e.target;
-      if (
-        !target.classList.contains("header__nav") &&
-        !target.classList.contains("header__burger")
-      ) {
-        link.classList.remove("active");
-        menu.classList.remove("open");
-      }
-    });
-  }
-  if (sectionTop && header) {
-    addPadTop(header, sectionTop)
-  }
-  let scroll = window.scrollY;
-  if (scroll > 50 ) {
-    header.classList.add("scroll");
-  } else {
-    header.classList.remove("scroll");
-  }
-
-
-  // Fancybox
-  Fancybox.bind("[data-fancybox]")
-
-});
-
-window.addEventListener("resize", () => {
-  if (sectionTop && header) {
-    addPadTop(header, sectionTop)
-  }
-});
-
-window.addEventListener("scroll", function () {
-  if(header) {
+  function handleScroll() {
     let scroll = window.scrollY;
-    if (scroll > 50 ) {
+    if (scroll > 50) {
       header.classList.add("scroll");
     } else {
       header.classList.remove("scroll");
     }
   }
+
+  function initializeMenuToggle(linkSelector, menuSelector) {
+    const link = document.querySelector(linkSelector);
+    const menu = document.querySelector(menuSelector);
+    
+    if (link && menu) {
+      const closeMenu = () => {
+        link.classList.remove("active");
+        menu.classList.remove("open");
+      };
+
+      link.addEventListener("click", function () {
+        link.classList.toggle("active");
+        menu.classList.toggle("open");
+
+        if (menu.classList.contains('header__box-menu')) {
+          header.classList.toggle("active");
+        }
+      }, false);
+
+      window.addEventListener("scroll", () => {
+        if (menu.classList.contains("open")) {
+          closeMenu();
+        }
+        handleScroll();
+      });
+
+      document.addEventListener("click", (e) => {
+        const target = e.target;
+        const isClickInsideMenu = menu.contains(target) || link.contains(target);
+
+        if (!isClickInsideMenu) {
+          closeMenu();
+        }
+      });
+    }
+  }
+
+  handleScroll();
+  window.addEventListener("scroll", handleScroll);
+
+
+  initializeMenuToggle('.menu-burger', '.header__box-menu');
+  initializeMenuToggle('.catalog-burger', '.header__box-catalog');
+
+  // Инициализация миниатюрного слайдера
+  const thumbsSlider = new Swiper('.thumbs-slider', {
+    slidesPerView: 1,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+
+  // Инициализация основного слайдера
+  const mainSlider = new Swiper('.main-slider', {
+    slidesPerView: 1,
+    effect: "fade",
+    thumbs: {
+        swiper: thumbsSlider,
+    },
+    navigation: {
+      nextEl: ".card__next",
+      prevEl: ".card__prev",
+    },
+    pagination: {
+      el: ".card__pagin",
+      clickable: true,
+    },
+  });
+  
 });
